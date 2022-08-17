9e0e962f06a549136517978d69683d7e2dd36970
terrykker <344163452@qq.com>
2022-08-17 14:06:28 +0800

    base基座和两个子应用

--- base/src/App.vue
+++ base/src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <br />
-  <router-link to="/">基座： Go to Home</router-link>
-  <br />
-  <router-link to="/mypage1">基座： 去子应用1</router-link>
-  <br />
-  <router-link to="/mypage2">基座： 去子应用2</router-link>
-  <br />
-  <router-view></router-view>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <br />
+    <router-link to="/">基座： Go to Home</router-link>
+    <br />
+    <router-link to="/mypage1">基座： 去子应用1</router-link>
+    <br />
+    <router-link to="/mypage2">基座： 去子应用2</router-link>
+    <br />
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
